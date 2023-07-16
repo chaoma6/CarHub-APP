38121494b8d64a52a856621d8abad34949285bb2
Chao Ma <machaosydney@gmail.com>
2023-07-16 23:09:35 +1000

    chore: add types/index.ts

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import type { MouseEventHandler } from 'react';
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: 'button' | 'submit';
+}
