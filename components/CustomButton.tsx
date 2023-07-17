dce16741840f30936d62e2f2b75e5daf4ec049e1
Chao Ma <machaosydney@gmail.com>
2023-07-18 01:46:00 +1000

    refactor: CustomButton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,23 +1,37 @@
 'use client';
 
+import Image from 'next/image';
 import React from 'react';
 
 import type { CustomButtonProps } from '@/types';
 
 const CustomButton = ({
+  isDisabled,
+  rightIcon,
   title,
   handleClick,
   containerStyles,
   btnType,
+  textStyles,
 }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled}
       type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative h-6 w-6">
+          <Image
+            src={rightIcon}
+            alt="arrow_left"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
